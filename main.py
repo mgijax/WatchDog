b2c0e7132f738d7cf76a4a35dba688c771b298fe
Olin Blodgett <nilo@nilosplace.net>
2015-05-05 13:29:52 -0400

    Added config files for server

--- main.py
+++ main.py
@@ -10,14 +10,15 @@ if __name__ == '__main__':
 	config = ConfigParser.ConfigParser()
 	config.readfp(open('host_setup.cfg'))
 	dictionary = {}
-	restclient = RestClient("10.20.22.241", 80)
 
 	for section in config.sections():
 		dictionary[section] = {}
 		for option in config.options(section):
 			dictionary[section][option] = config.get(section, option)
 
-	collector = LinuxDataPointCollector(dictionary['config'])
+	cfg = dictionary['config']
+	restclient = RestClient(cfg['collector_server_name'], cfg['collector_server_port'])
+	collector = LinuxDataPointCollector(cfg)
 
 	while True:
 		points = collector.runCommands()
