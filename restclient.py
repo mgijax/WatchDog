29472923af19359013d26b1c4545f38e2f8d46d0
MGI Software Installer <mgiadmin@mgi-testweb3.jax.org>
2015-05-05 14:36:31 -0400

    Changed server endpoint

--- restclient.py
+++ restclient.py
@@ -8,7 +8,7 @@ class RestClient:
 		self.baseurl = "/rest"
 
 	def senddatapoint(self, json_string):
-		endpoint = "/serverdata"
+		endpoint = "/datapoint"
 		connection = httplib.HTTPConnection(self.restserver, self.restport)
 		connection.connect()
 		connection.request('POST', self.baseurl + endpoint, json.dumps(json.loads(json_string)), {"Content-Type": "application/json"})
