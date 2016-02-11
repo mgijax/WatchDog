95c05179aefe3182fe663ed06593307b73c2e855
MGI Software Installer <mgiadmin@bhmgiapt02.jax.org>
2016-02-11 11:03:20 -0500

    Added fix for production tags

--- systemcommands/systeminfo.py
+++ systemcommands/systeminfo.py
@@ -1,4 +1,5 @@
 import os
+import re
 from datapoint import *
 from command import Command
 
@@ -13,6 +14,8 @@ class SystemInfo(Command):
 		list = []
 		packages = {}
 		self.recurse("/usr/local/mgi/live", list)
+		if self.debug:
+			print list
 		for d in list:
 			if not os.path.islink(d):
 				if self.debug:
@@ -34,7 +37,7 @@ class SystemInfo(Command):
 						packages[product] = []
 					packages[product].append(tag)
 				else:
-					name = os.path.basename(d).split("-")
+					name = os.path.basename(d).split("-", 1)
 					product = name[0]
 					if len(name) > 1:
 						tag = name[1]
@@ -61,6 +64,10 @@ class SystemInfo(Command):
 		else:
 			for d in dirs:
 				newpath = dir + "/" + d
-				if os.path.isdir(newpath):
-					self.recurse(newpath, list)
+				m = re.search('\w*-\d-\d-\d-\d', d)
+				if m:
+					list.append(dir + "/" + d)
+				else:
+					if os.path.isdir(newpath):
+						self.recurse(newpath, list)
 
